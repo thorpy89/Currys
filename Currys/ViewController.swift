0e7bda518585cb078b5241020eed5a6728921eb9
David <davidthorp@davids-mbp.lan>
2017-04-06 22:13:52 +0100

    First upload
    First time uploading to git

--- Currys/ViewController.swift
+++ Currys/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
+        let url = NSURL (string: "http://www.currys.co.uk/gbuk/index.html")
+        let requestObj = NSURLRequest(url: url! as URL);
+        webView.loadRequest(requestObj as URLRequest)
+          }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
